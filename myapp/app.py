aa8f1fdd40a7c3b8ea10b414b50fad002d4b3383
justinfederico <jfederico0241@sdsu.edu>
2022-05-02 23:47:07 -0700

    Functional.

--- myapp/app.py
+++ myapp/app.py
@@ -1,20 +1,78 @@
 from flask import Flask, render_template, request, url_for
-import flask_mqtt
-from datetime import datetime
+import paho.mqtt.client as mqtt
 
 app = Flask(__name__)
-# app.config['MQTT_BROKER_URL'] = 'mybroker.com'
+topic = '/raspberrypi/matrix/links'
+port = 1883
+
+
+# app.config['MQTT_BROKER_URL'] = 'broker.emqx.io'
 # app.config['MQTT_BROKER_PORT'] = 1883
-# app.config['MQTT_USERNAME'] = 'user'
-# app.config['MQTT_PASSWORD'] = 'secret'
+# app.config['MQTT_USERNAME'] = ''
+# app.config['MQTT_PASSWORD'] = ''
 # app.config['MQTT_REFRESH_TIME'] = 1.0  # refresh time in seconds
-#mqtt = flask_mqtt.Mqtt(app)
+# # Parameters for SSL enabled
+#
+# # app.config['MQTT_TLS_ENABLED'] = True
+# # app.config['MQTT_TLS_INSECURE'] = True
+# # app.config['MQTT_TLS_CA_CERTS'] = 'broker.emqx.io-ca.crt'
 
 
-@app.route("/")
+def on_connect(client, userdata, flags, rc):
+    client.subscribe(topic)
+    print('connected')
+
+
+def on_message(client, userdata, msg):
+    print("message received")
+
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
+    if request.method == "POST":
+        image = request.form.get("image")
+        imagePi = 'I:' + image
+        client.publish(topic, imagePi)
+
     return render_template('index.html')
 
 
+# @mqtt.on_connect()
+# def handle_connect(client, userdata, flags, rc):
+#     print("connected!")
+#     mqtt.subscribe('/raspberrypi/matrix/links')
+#     mqtt.publish('/raspberrypi/matrix/links', 'I:https://art.pixilart.com/79dcbd564094d2e.png')
+#
+#
+# # @socketio.on('publish')
+# # def handle_publish(imagePi):
+# #     mqtt.publish('/raspberrypi/matrix/links', imagePi)
+#
+#
+# # @socketio.on('subscribe')
+# # def handle_subscribe():
+# #     mqtt.subscribe('/raspberrypi/matrix/links')
+# #
+# #
+# # @socketio.on('unsubscribe_all')
+# # def handle_unsubscribe_all():
+# #     mqtt.unsubscribe_all()
+#
+#
+# @mqtt.on_message()
+# def handle_mqtt_message(client, userdata, message):
+#     data = dict(
+#         topic=message.topic,
+#         payload=message.payload.decode()
+#     )
+#     socketio.emit('mqtt_message', data=data)
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    client = mqtt.Client()
+    # client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect('broker.emqx.io', port, 60)
+    client.loop_start()
+    app.run(host='0.0.0.0')
